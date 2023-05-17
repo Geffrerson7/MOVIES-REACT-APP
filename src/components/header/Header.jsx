8c6a1d41b931e71982340af87fab97758bcbf257
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-17 16:02:56 -0500

    Add Register and Login page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,19 @@
 import './Header.css'
 import { Link } from "react-router-dom";
 import React from 'react'
+import Logout from '../logout/Logout';
 
 const Header = () => {
   return (
     <div className='header'>
-        <div>
-            <Link to="/"><img className='header__icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
-            <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-            <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-            <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
-        </div>
+      <div className='header__links'>
+        <Link to="/"><img className='header__icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
+        <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+        <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+        <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+        <Logout />
+      </div>
     </div>
   )
 }
-
 export default Header
