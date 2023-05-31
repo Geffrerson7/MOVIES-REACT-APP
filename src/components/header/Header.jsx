34369259de68f664a503b316eb3b9e975bd0b06b
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-31 12:45:04 -0500

    Add new icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,13 @@ import './Header.css'
 import { Link } from "react-router-dom";
 import React from 'react'
 import Logout from '../logout/Logout';
+import iconImg from '../../assets/img/icon.png'
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header__links'>
-        <Link to="/"><img className='header__icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
+        <Link to="/"><img className='header__icon' src={iconImg} /></Link>
         <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
         <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
         <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
